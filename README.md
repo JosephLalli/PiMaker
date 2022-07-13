944454f34e381050d2e090777707cde509acb824
JosephLalli <lalli@wisc.edu>
2022-07-13 12:27:16 -0500

    Updating readme, making lack of FST support clear.

--- README.md
+++ README.md
@@ -9,4 +9,23 @@ To install, simply navigate to your project's folder and clone this repository u
 
 You can then import pimaker into your project as you wish.
 
+If you would like to use the command line, please run:
+
+    chmod +x /path/to/pimaker.py
+
 Further instructions on use are under development, though you are encouraged to look through the code for function documentation.
+
+### Example use:
+    VCF="path/to/vcf/file/of/all/samples"
+    REF="path/to/fasta/reference/VCF/was/called/against"
+    GTF="path/to/GFF3/or/GTF/annotation/file"
+
+    optional:
+    RATES='path_to_csv_file/of_4x4_matrix_(ACGT ordered)/of_relative_all_to_all_mutation_rates.csv'
+
+    run command:
+    python pimaker.py -v $VCF -g $GTF -r $REF --mutation_rates $RATES -t 8
+
+### Memory management
+PiMaker is designed to load all data being computed into memory. If this causes out of memory errors, we suggest altering the chunksize value to reduce the memory usage of the data that each thread is processing. To do this, change the "--chunk_size" value. Default is 1000000 bases per chunk. Increasing this value will moderately increase speed. Reducing this value will reduce peak memory usage almost linearly.
+
--- pimaker/args.py
+++ pimaker/args.py
@@ -34,8 +34,8 @@ def get_parser():
     parser.add_argument('-o', '--output', required=True, type=str, default='pimaker/result',
                         help='''Location of output files. Sample, Gene, and Site specific results will each have
                         this prefix + \'_gene.csv\', etc. attached. defaults to \'pimaker/result\'''')
-    parser.add_argument('--FST', type=bool, action='store_true',
-                        help='''Calculate pairwise FST, synonymous FST, and nonsynonymous FST for all sample pairs.''')
+    # parser.add_argument('--FST', type=bool, action='store_true',
+    #                     help='''Calculate pairwise FST, synonymous FST, and nonsynonymous FST for all sample pairs.''')
     parser.add_argument('--maf', type=float, default=0.0,
                         help='''Minimum intra-sample frequency of variants to be considered when calculating
                                 diversity''')
@@ -43,22 +43,23 @@ def get_parser():
                         help='''Location of csv file containing empirically defined mutation rates. In the
                                 absence of specified mutation rates, PiMaker assumes all mutations are equally
                                 likely to occur as in Nei & Gojobori, 1986''')
-    parser.add_argument('--rolling_window', type=int, default=0,
-                        help='''How wide should the rolling window be for a rolling window analysis of pi/piN/piS''')
+    # parser.add_argument('--rolling_window', type=int, default=0,
+    #                     help='''How wide should the rolling window be for a rolling window analysis of pi/piN/piS''')
     parser.add_argument('--pi_only', action='store_true',
                         help='''Most of the processing time for this script is spent on calculating synonymous
                                 and nonsynonymous-specific mutations. Use this flag to skip this analysis and just
                                 calculate pi''')
     parser.add_argument('--include_stop_codons', action='store_true',
-                        help='''Nei and Gojobori 1986 assume that mutations to stop codons do not randomly.
-                                For some populations (especially rapidly adaptive populations), this assumption may
-                                not be valid''')
+                        help='''Nei and Gojobori 1986 assume that mutations to stop codons cause a drop to zero fitness
+                                and therefore may be ignored. For some populations (especially rapidly adaptive populations), this assumption may
+                                not be valid. By default follows Nei and Gojobori rules; use this option to consider stop
+                                codons as nonsynonymous mutations.''')
     parser.add_argument('--chunk_size', type=int,
                         help='''Number of nucleotides to process at a time per thread. Higher chunk_size equals
                                 faster performance with more memory usage. Lower chunk_size results in (slightly) slower
                                 performance with less memory usage. Default value is 1,000,000 nucleotides per analyzed
                                 chunk. Please adjust this first if you are encoutering errors due to high memory usage''', default=int(1e6))
-    parser.add_argument('-t', '--threads', type=int, default=os.cpu_count() - 1,
+    parser.add_argument('-t', '--threads', type=int, default= 1,
                         help='''Number of processes to use while performing calculations''')
     return parser
 
